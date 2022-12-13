37a500b78189cef0a727d08c789110eca7e859a0
mahmoud ramadan <mahmoudr03@gmail.com>
2022-12-13 14:13:06 +0200

    finish code

--- js/main.js
+++ js/main.js
@@ -31,3 +31,71 @@ const toggleMenuOpen = () => document.body.classList.toggle("open");
 //         isMenuAlreadyOpen = !isMenuAlreadyOpen
 //     })
 // });
+
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+      } else {
+        element.classList.remove('is-visible');
+      }
+    });
+
+    scroll(loop);
+}
+loop();
+
+function isElementInViewport(el) {
+  
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+
+// let photo= document.querySelector('.photo');
+
+// function scrollApp(e) {
+//     if(window.scrollY >= e.offsetTop) {
+//         element.css('opacity','1');
+//     } else {
+//         element.css('opacity','0');
+
+//     }
+// }
+// scrollApp(photo)
+
+    $(window).scroll(function () {
+    if ($(this).scrollDown() > 1726) {
+      $('.photo').animate({
+        with: "4500px"
+      },500);
+    } else {
+      $('.photo').fadeOut('slow');
+    }
+  });
+
+  $('.containerScroll').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
